0ec76be9c5d680b1847ee198fd74fa83be6aabea
thaheshan suresh <166691277+thaheshan@users.noreply.github.com>
2025-01-31 22:32:13 +0530

    importing necessary python libraries and fundamentals for the hand sign detection model requirements

---
+++ photo_model_train.py
@@ -0,0 +1,19 @@
+#import cv2 for the opencv hand detection libraries.
+import cv2
+
+#import this for ecpecially import the requirement libaries for hand detection and detect boundaries with landmarks.
+from cvzone.HandTrackingModule import HandDetector
+
+#import the classifier from the cvzone, because of it's requirement for the hand gesture recognition libraries.
+from cvzone.ClassificationModule import Classifier
+
+#import the numpy python library for the numerical calculation operations
+import numpy as np
+
+#import the math for the calculations
+import math
+
+#import the time for the video frame reate contro with the video or frame capture with it's real live time stamps
+import time
+
+
