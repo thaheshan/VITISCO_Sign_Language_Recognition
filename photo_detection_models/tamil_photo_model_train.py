eed6dc293232c6a1ad874ec7a70e505d1dab9ec0
Zheong10 <zuhar.20232323@iit.ac.lk>
2025-02-09 23:47:57 +0530

    Trained the model and interconnected photo detection
    Trained the models for tamil letters and interconnected photo detection for checking purpose

--- photo_detection_models/tamil_photo_model_train.py
+++ photo_detection_models/tamil_photo_model_train.py
@@ -30,7 +30,7 @@ detector = HandDetector(maxHands=2)
 #here i imported the needed files for the gesture recongnitions
 #i imported the well-trained model with it's suitable lables for the gesture recognition
 # A well-trained keras recogition model imported here with it's suitable labels
-classifier = Classifier("models/keras_model.h5", "models/labels.txt")
+classifier = Classifier("../keras_model.h5", "../labels.txt")
 
 
 
