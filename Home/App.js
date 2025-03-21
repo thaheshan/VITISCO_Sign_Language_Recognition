31d4b50dbdbae441f1d24dfd82d3261838e64300
not-muaaza <muaaza.20221218@iit.ac.lk>
2025-03-21 16:37:07 +0530

    changes

--- Home/App.js
+++ Home/App.js
@@ -7,12 +7,12 @@ import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import HomeScreen from "./HomeScreen.js";
 import QuizScreen from "./Screens/Quiz.js";
 import VirtualRoomScreen from "./Screens/VirtualRoom.js";
-//import LessonScreen from  "./Screens/Lesson.js";
+import LessonScreen from  "./Screens/Lesson.js";
 import ProgressAnalysisScreen from "./Screens/ProgressAnalysis.js";
 import ProfileScreen from "./Screens/Profile.js";
 import SettingsScreen from "./Screens/Settings.js";
-//import TranslatorScreen from "./Screens/Translator.js";
-//import TextToSpeechScreen from "./Screens/TexttoSpeech.js";
+import TranslatorScreen from "./Screens/Translator.js";
+import TextToSpeechScreen from "./Screens/TexttoSpeech.js";
 import NotificationScreen from "./Screens/Feedback.js";
 import LoginScreen from "./Screens/Login.js";
 import SchedulerScreen from  "./Screens/schduler.js";
@@ -52,14 +52,14 @@ const App = () => {
       <Stack.Navigator initialRouteName={ "Home" }>
         <Stack.Screen name="Login" component={LoginScreen} options={{ headerShown: false }} />
         <Stack.Screen name="Home" component={HomeScreen} options={{ headerShown: false }} />
-        {/* <Stack.Screen name="Lesson" component={LessonScreen} options={{ title: "Lesson Details" }} /> */}
+        <Stack.Screen name="Lesson" component={LessonScreen} options={{ title: "Lesson Details" }} /> 
         <Stack.Screen name="Quiz" component={QuizScreen} options={{ title: "Quiz Details" }} />
         <Stack.Screen name="VirtualRoom" component={VirtualRoomScreen} options={{ title: "Virtual Room" }} />
         <Stack.Screen name="ProgressAnalysis" component={ProgressAnalysisScreen} options={{ title: "Progress Analysis" }} />
         <Stack.Screen name="Profile" component={ProfileScreen} />
         <Stack.Screen name="Settings" component={SettingsScreen} />
-         {/* <Stack.Screen name="Translator" component={TranslatorScreen} />  */}
-         {/* <Stack.Screen name="TextToSpeech" component={TextToSpeechScreen} />  */}
+        <Stack.Screen name="Translator" component={TranslatorScreen} />  
+         <Stack.Screen name="TextToSpeech" component={TextToSpeechScreen} /> 
         <Stack.Screen name="Notifications" component={NotificationScreen} />
         <Stack.Screen name="Scheduler" component={SchedulerScreen} />
       </Stack.Navigator>
